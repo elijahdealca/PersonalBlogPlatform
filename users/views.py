d1fc47f7fcee8de5ba18a4dbd98501aaddb186a7
elijahdealca <edealca26@adrian.edu>
2025-03-23 15:35:24 -0400

    Sprint 2 - Added BlogPost CRUD functionality and views

--- users/views.py
+++ users/views.py
@@ -1,10 +1,16 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
+from .models import BlogPost
+from .forms import BlogPostForm
+
+# Home view
 def home(request):
     return render(request, "users/home.html")
+
 # User registration
 def register(request):
     if request.method == "POST":
@@ -13,7 +19,7 @@ def register(request):
             user = form.save()
             login(request, user)
             messages.success(request, "Registration successful!")
-            return redirect("home")  # Ensure "home" is a valid URL name
+            return redirect("home")
     else:
         form = UserCreationForm()
     return render(request, "users/register.html", {"form": form})
@@ -22,7 +28,7 @@ def register(request):
 def login_view(request):
     form = AuthenticationForm()
     if request.method == "POST":
-        form = AuthenticationForm(data=request.POST)  # Built-in Django form
+        form = AuthenticationForm(data=request.POST)
         if form.is_valid():
             user = form.get_user()
             login(request, user)
@@ -30,7 +36,6 @@ def login_view(request):
             return redirect("home")
         else:
             messages.error(request, "Invalid credentials")
-
     return render(request, "users/login.html", {"form": form})
 
 # User logout
@@ -38,3 +43,47 @@ def logout_view(request):
     logout(request)
     messages.info(request, "You have been logged out.")
     return redirect("login")
+
+# Blog post views
+
+def post_list(request):
+    posts = BlogPost.objects.all().order_by('-created_at')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(BlogPost, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+@login_required
+def post_create(request):
+    if request.method == "POST":
+        form = BlogPostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = BlogPostForm()
+    return render(request, 'blog/post_form.html', {'form': form})
+
+@login_required
+def post_edit(request, pk):
+    post = get_object_or_404(BlogPost, pk=pk)
+    if request.user != post.author:
+        return redirect('post_list')
+    if request.method == "POST":
+        form = BlogPostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = BlogPostForm(instance=post)
+    return render(request, 'blog/post_form.html', {'form': form})
+
+@login_required
+def post_delete(request, pk):
+    post = get_object_or_404(BlogPost, pk=pk)
+    if request.user == post.author:
+        post.delete()
+    return redirect('post_list')
