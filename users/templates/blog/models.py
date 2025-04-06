811331203c34ef65324f79b1d6cbb9dc31b7d6ac
elijahdealca <edealca26@adrian.edu>
2025-04-06 16:33:37 -0400

    Sprit 3

---
+++ users/templates/blog/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Comment(models.Model):
+    post = models.ForeignKey(BlogPost, related_name="comments", on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    image = models.ImageField(upload_to='profile_pics/', default='default.jpg')
+    twitter = models.URLField(blank=True)
+    linkedin = models.URLField(blank=True)
